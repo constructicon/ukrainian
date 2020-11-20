79fb8ae92a0670b9a79baa798696fe826182c0ed
Radovan Bast <bast@users.noreply.github.com>
2020-11-20 14:10:48 +0100

    rm a leftover console.log

--- static/js/constructicon.js
+++ static/js/constructicon.js
@@ -38,7 +38,6 @@ function build_tree_for_advanced_search(record_numbers, records, key) {
 
 
 async function fetch_data(data, url_prefix) {
-    console.log(url_prefix + 'data-combined.yml');
     let r = await axios.get(url_prefix + 'data-combined.yml');
     var json_data = jsyaml.load(r.data);
 
